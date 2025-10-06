199974a45c0b9903b8333401fc0d0e36ee020f94
nikul1n <nikul1n@bk.ru>
2025-10-06 21:24:08 +0300

    cikli

---
+++ task_6.2.py
@@ -0,0 +1,10 @@
+number = int(input())
+
+if number == 7:
+    print("Goodbye!")
+elif number == 0:
+    print("Number is equal zero")
+elif number < 0:
+    print("Number is negative")
+else:
+    print("Number is positive")
