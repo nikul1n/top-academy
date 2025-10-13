d3d5acd0b10023df959db06e5e7055af927d392f
nikul1n <nikul1n@bk.ru>
2025-10-13 19:02:10 +0300

    new table multiple

--- table_multiple.py
+++ table_multiple.py
@@ -1,4 +1,5 @@
-number = int(input())
+number = int(input("Введите число умножения: "))
+columns = int(input("Введите число, до которого выводить умножение: "))
 
-for n in range(1, 11):
+for n in range(1, columns + 1):
     print(f"{number}*{n} = {number*n}")
