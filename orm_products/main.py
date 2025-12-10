d93e812a36d2b89e4c99e6a4943938e0c0a97a08
nikul1n <nikul1n@bk.ru>
2025-12-10 21:11:11 +0300

    create table products

---
+++ orm_products/main.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped
+from sqlalchemy.orm import mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    id:Mapped[int] = mapped_column(primary_key=True)
+
+
+class Product(Base):
+    __tablename__ = "products"
+    product_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    price: Mapped[float] = mapped_column(nullable=False)
+    quanitity: Mapped[int] = mapped_column(nullable=False) #количество
+    categories: Mapped[Optional["Category"]] = relationship(
+        back_populates="products"
+        )
+
+# cascade="all, delete-orphan"
+# cascade - сделать теже самые 
+
+class Category(Base):
+    __tablename__ = "categories"
+    category_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    product_id: Mapped[int] = mapped_column(ForeignKey="products.id")
+    products: Mapped[List[Product]] = mapped_column(back_populates="categories", cascade="save-update")
+
+# Остаток цена название ид
--- sql/orm_simple.py
+++ sql/orm_simple.py
@@ -1,41 +1,69 @@
 # pip install sqlalchemy
 # pip install psycopg2
+# set client_encoding='win1251';
 
 from typing import List
 from typing import Optional
 from sqlalchemy.orm import Session
-from sqlalchemy import ForeignKey, create_engine, select
+from sqlalchemy import ForeignKey, create_engine, delete, select
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import mapped_column, sessionmaker, relationship
+from sqlalchemy.ext.asyncio import create_async_engine
 
 engine = create_engine("postgresql://postgres:admin@localhost/p51_users_test")
 
-def main():
-    Base.metadata.create_all(engine)
-    with Session(engine) as session:
-        if session.query(User).count() == 0:
-            session.add_all(
-                [
-                User(name="sandy", fullname="Sandy Cheeks"),
-                User(name="jim", fullname="Jim Jones"),
-                User(name="cathy", fullname="Cathy Carson"),     
-                ]
-            )
-            session.commit()
+create_session = sessionmaker(bind=engine)
 
-        for user in session.scalars(select(User)):
-            print(user)
-                                     
+def main():
+    with create_session() as session: #создаётся сессия которая идёт как одна 
+        # транзакция (набор sql команд которые можно откатить)
+        session.execute(delete(User))
+        session.commit() #коммит посылает все исполненные команды
 
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[int] = mapped_column(primary_key=True) #все базы будут иметь id.
+    # Mapped специальный тип который будет маппить тип в квадратных скобках (основа для других таблиц)
+
+class User(Base):
+    __tablename__ = "user_account"
+    name: Mapped[str] = mapped_column(String(30), unique=True)
+    fullname: Mapped[Optional[str]]
+    addresses: Mapped[List["Address"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped["User"] = relationship(back_populates="addresses")    
+
+# Остаток цена название ид
+
+    # Base.metadata.create_all(engine)
+    # with Session(engine) as session:
+    #     if session.query(User).count() == 0:
+    #         session.add_all(
+    #             [
+    #             User(name="sandy", fullname="Sandy Cheeks"),
+    #             User(name="jim", fullname="Jim Jones"),
+    #             User(name="cathy", fullname="Cathy Carson"),     
+    #             ]
+    #         )
+    #         session.commit()
+
+    #     for user in session.scalars(select(User)):
+    #         print(user)
+                                     
+
 
 class User(Base):
     __tablename__ = "user_account"
     id: Mapped[int] = mapped_column(primary_key=True)
+
     name: Mapped[str] = mapped_column(String(30))
     fullname: Mapped[Optional[str]]
     addresses: Mapped[List["Address"]] = relationship(
