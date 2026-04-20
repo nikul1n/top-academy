e9193909ec24ed9fdb9d05cf057fae3e25469643
nikul1n <nikul1n@bk.ru>
2026-04-20 21:15:38 +0300

    memo

---
+++ element/ColorSwitch.js
@@ -0,0 +1,37 @@
+export class ColorSwitcher {
+    constructor(element, colors, initialColorIndex) {
+        this._element = element;
+        this._colors = colors;
+        // this._colorIndex = initialColorIndex; // % ColorSwitcher.colors.length;
+        this.setColor(initialColorIndex); // this._element.style.backgroundColor = this._colors[this.colorIndex];
+        this.addClickHandler();
+    }
+
+
+    setColor (index) {
+        this._colorIndex = index;
+        if (this._colorIndex < 0 || this._colorIndex > this._colors.length) {
+            throw new Error("initialColorIndex index is out of colors boundaries");
+        }
+        this._element.style.backgroundColor = this._colors[this._colorIndex];
+    }
+
+    reset () {
+        this.setColor(0);
+    }
+
+    // static backgroundColorList = ['red', 'blue', 'green'];
+
+    addClickHandler() {
+        // colorIndex = 0;
+        this._element.addEventListener('click', () => {
+            this._colorIndex++;
+            if (this._colorIndex >= this._colors.length) {
+                this._colorIndex = 0;
+            }
+            this.setColor(this._colorIndex); // this._element.style.backgroundColor = this._colors[this.colorIndex];
+        });
+    }
+
+    getElement() { return this._element; }
+}
--- element/script.js
+++ element/script.js
@@ -1,27 +1,25 @@
-'use strict';
+"use strict";
 
-class ColorSwitcher {
-    constructor(element) {
-        this._element = element;
-        this.addClickHandler();
-        this._element.style.width = "100px";
-        this._element.style.height = "100px";
+import { ColorSwitcher } from "./ColorSwitch.js";
+
+
+
+const blocksList = document.querySelector(".blocks-list");
+const switchers = [];
+const switchersCount = 4 * 4;
+
+for (let i = 0; i < switchersCount; i++) {
+    const element = document.createElement('div');
+    element.classList.add('colored-block');
+    const switcher = new ColorSwitcher(
+        element, ['hsl(0, 70%, 80%)', 'hsl(180, 70%, 80%)'], Math.floor(Math.random() * 2));  //Math.floor(Math.random() * 10)); //document.createElement('div'));
+        switchers.push(switcher);
+        blocksList.appendChild(switcher.getElement());
     }
+
     
-    static backgroundColorList = ['red', 'blue', 'green'];
-
-    addClickHandler() {
-        let i = 0;
-        this._element.addEventListener('click', () => {
-            i++;
-            if (i == ColorSwitcher.backgroundColorList.length) {
-                i = 0;
-            }
-            this._element.style.backgroundColor = ColorSwitcher.backgroundColorList[i];
-            // this.style.backgroundColor = `${colorList[i]}`;
-        });
-    }
-}
-
-
-const switcher = new ColorSwitcher(document.getElementById("button-switcher-color"));
+    // const switcher = new ColorSwitcher(
+    //     document.createElement('div'));
+    // const switcher1 = new ColorSwitcher(document.getElementById("button-switcher-color-1"));
+    
+    // blocksList.appendChild(switcher.getElement());
